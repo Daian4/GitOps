ffbe2cbddd4525d34110e8c129f9b683007b462d
Daiana L <daianalimaep@gmail.com>
2025-11-12 15:27:50 -0300

    feat: add webserver

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello Argo</h1>"))
+	})
+	http.ListenAndServe(":5500", nil)
+}
